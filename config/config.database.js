1672648ce0a7b3de11bbba99695ede28d4428905
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-03-10 15:20:34 +0100

    fixes

--- config/config.database.js
+++ config/config.database.js
@@ -1,20 +1,22 @@
 require('dotenv').config()
 const mysql = require('mysql2');
-
-//create database connection
-const database = mysql.createConnection({
-    host:process.env.DB_HOST,
+const databaseConnectionOptions = {
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password:process.env.DB_ACCESS_KEY,
-    database:process.env.DB_SCHEMA
-})
+    password: process.env.DB_ACCESS_KEY,
+    database: process.env.DB_SCHEMA
+};
 
+//create database connection instance 
+const database = mysql.createConnection(databaseConnectionOptions)
 
-//init database trasaction
 database.connect(err => {
     if (err) console.error('error connecting: ' + err.stack);
     console.log('connected as id ' + database.threadId);
 });
 
-
-module.exports = database ;
+//modularize connection option and database connection
+module.exports = {
+    databaseConnectionOptions,
+    database
+}
