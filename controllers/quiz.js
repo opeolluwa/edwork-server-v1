bf26538d2436119a18ac53201e60eb3bd73d4f90
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-03-28 14:58:04 +0100

    update

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,6 +1,6 @@
 const { database } = require("../config/config.database")
 const { randomQuestions } = require("../utils/nRandomItems")
-
+global.score = 0;
 
 
 function quizGenerator(req, res) {
@@ -14,7 +14,7 @@ function quizGenerator(req, res) {
             .then(([rows, fields]) => {
                 //extract 50 random questions from the result
                 //filter the response to be sent, send only question and option, keep a reference to the answer in the user table using a uniques id
-                const questions = randomQuestions(rows, 50)
+                const questions = randomQuestions(rows, 70)
                     .map((questions) => {
                         /*   {
                          "id": 194,
@@ -46,13 +46,46 @@ function quizGenerator(req, res) {
     }
 }
 
-function quizMarker(req, res) {
+function quizMarker(req, res, next) {
     //get payload from client
     const { answers: idAndAnswersIndex, subject } = req.body
-    console.log(subject, idAndAnswersIndex);
+    const idOfAttemptedQuestion = Object.keys(idAndAnswersIndex)
+    // console.log(subject, idAndAnswersIndex);
+//[3,65, 45,102]
+    /*    idOfAttemptedQuestion.forEach(element => {
+           database
+               .promise()
+               .query(`SELECT answer FROM ${subject.toLowerCase()}_question_bank WHERE (id =? )`, [element])
+               .then(([rows, fields]) => {
+                   if (rows[0].answer == idAndAnswersIndex[element]) {
+                       counter += 1;
+                   }
+               })
+   
+       }); */
+    var counter = 0;
+    // console.log(counter);
+    for (const element of idOfAttemptedQuestion) {
+        database
+            .promise()
+            .query(`SELECT answer FROM ${subject.toLowerCase()}_question_bank WHERE (id =? )`, [element])
+            .then(([rows, fields]) => {
+                counter = 0
+
+                if (rows[0].answer == idAndAnswersIndex[element]) {
+                    counter += 1;
+                    // console.log(counter);
+
+                }
+            })
+    }
+    console.log(counter);
 }
 
-module.exports = { quizGenerator, quizMarker}
+
+
+
+module.exports = { quizGenerator, quizMarker }
 
 
 //a function that takes in a number an <array> as first argz and a <size> as second and return  
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const cors = require('cors')
-const { quizGenerator, quizMarker } = require("../controllers/quiz");
+const { quizGenerator, quizMarker, } = require("../controllers/quiz");
 
 
 router.post("/", quizGenerator);
-router.post("/submit", quizMarker);
+router.post("/submit", quizMarker, ((req, res) => {
+    console.log(req.score);
+}));
 
 router.use(cors())
 
