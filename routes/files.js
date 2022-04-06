9a08df81216b5bf0ff4b39768db0144b9353854b
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-04-06 14:12:39 +0100

    CHORE :: update

--- routes/files.js
+++ routes/files.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-const { search, add } = require('../controllers/files')
+const { search, add, getRelatedSearchResult } = require('../controllers/files')
 const { validate_search_query, validate_search_add_files } = require('../middleware')
 
 router.use(cors())
 router.post('/search',/*  validate_search_query, */ search)
 router.post('/add', validate_search_add_files, add)
+router.get("/search/results/:id", getRelatedSearchResult)
 module.exports = router
